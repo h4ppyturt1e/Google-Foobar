919bfc9a8dddf2658db470b915c20b9c55da6420
h4ppyturt1e <ryan170402@gmail.com>
2024-01-23 18:49:13 -0800

    final solution, used XOR modulo tricks

--- Level 3/queue-to-do/solution.py
+++ Level 3/queue-to-do/solution.py
@@ -1,24 +1,42 @@
-
-from functools import reduce
-
-
 def solution(start, length):
     if length == 0:
         return 0
     
-    toBeXORed = []
+    res = 0
     originalLength = length
     for i in range(originalLength):
-        toBeXORed.extend(list(range(start, start + length - i)))
+        xor = getXORRange(start, start + length - i - 1)
         start += length
-        # length -= 1
-    
-    # XOR all the numbers in the list
-    # print(toBeXORed)
-    res = reduce(lambda x, y: x ^ y, toBeXORed)
-    
+        res ^= xor
+        
     return res
 
+def getXORRange(start, end):
+    # get 0 to start - 1
+    zeroToStart = -1
+    if (start - 1) % 4 == 0:
+        zeroToStart = start - 1
+    elif (start - 1) % 4 == 1:
+        zeroToStart = 1
+    elif (start - 1) % 4 == 2:
+        zeroToStart = start
+    else: # (start - 1) % 4 == 3:
+        zeroToStart = 0
+    
+    # get 0 to end
+    zeroToEnd = -1
+    if end % 4 == 0:
+        zeroToEnd = end
+    elif end % 4 == 1:
+        zeroToEnd = 1
+    elif end % 4 == 2:
+        zeroToEnd = end + 1
+    else: # end % 4 == 3:
+        zeroToEnd = 0
+    
+    return zeroToStart ^ zeroToEnd
+    
+    
 tests = [(0, 3), (17, 4), (0, 0), (10, 4), (0, 4), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5)]
 
 for test in tests:
